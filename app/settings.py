119b7a68c5ee6ec7d3f60f1943ae29bec2667dc4
Arthur Abreu <arthuraudayadreu@gmail.com>
2026-04-23 22:51:13 -0300

    Fix crashed

--- app/settings.py
+++ app/settings.py
@@ -75,11 +75,11 @@ WSGI_APPLICATION = 'app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME':os.getenv('agenda'),
-        'USER':os.getenv('postgres'),
-        'PASSWORD':os.getenv('Python123'),
-        'HOST':os.getenv('localhost'),
-        'PORT':os.getenv('5432'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
