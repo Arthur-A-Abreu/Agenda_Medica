a3384d4e5e714778b039f82867c4105f48074a73
Arthur Abreu <arthuraudayadreu@gmail.com>
2026-04-24 19:57:11 -0300

    Fix erros

--- app/settings.py
+++ app/settings.py
@@ -4,6 +4,7 @@ Django settings for app project.
 
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,18 +77,13 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME', 'neondb'),
-        'USER': os.getenv('DB_USER', 'neondb_owner'),
-        'PASSWORD': os.getenv('NEON_PASSWORD', ''),
-        'HOST': os.getenv('DB_HOST', 'ep-lively-mode-and6xs6f-pooler.c-6.us-east-1.aws.neon.tech'),
-        'PORT': '5432',
-        'OPTIONS': {
-            'sslmode': 'require',
-        },
-    }
+    'default': dj_database_url.parse(
+        os.getenv('NEON_DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True,
+    )
 }
 
 
